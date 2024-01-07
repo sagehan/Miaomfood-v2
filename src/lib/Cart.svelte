7e991bbe733d59288f54f1403ec9e1be96b62eb7
Sage <zongshian@outlook.com>
2024-01-07 11:44:14 +0800

    [UI] use flexbox to lay out the cart table, no cell alignment yet.

--- src/lib/Cart.svelte
+++ src/lib/Cart.svelte
@@ -13,40 +13,59 @@
 	<h1>购物车</h1>
 
 	<form>
-		<table class="cart-items">
+		<table role="table" id="cart-items">
 			<caption class="step__hd">订购信息</caption>
-			<thead>
-				<tr>
-					<th>餐品</th>
-					<th>单价</th>
-					<th>数量</th>
-					<th>金额</th>
+			<thead role="rowgroup">
+				<tr role="row">
+					<th role="columnheader">餐品</th>
+					<th role="columnheader">单价</th>
+					<th role="columnheader">数量</th>
+					<th role="columnheader">金额</th>
 				</tr>
 			</thead>
-			<tbody>
+			<tbody role="rowgroup">
 				<template>
-					<tr class="h-item">
-						<td class="p-name">土耳其烤鸡肉披萨</td>
-						<td class="spec-price number">69</td>
-						<td class="qty number">2</td>
-						<td class="sub-total number">138</td>
+					<tr role="row" class="h-item">
+						<td role="cell" class="p-name">土耳其烤鸡肉披萨</td>
+						<td role="cell" class="spec-price number">69</td>
+						<td role="cell" class="qty number">2</td>
+						<td role="cell" class="sub-total number">138</td>
 					</tr>
 				</template>
 			</tbody>
-			<tfoot>
-				<tr><th colspan="4">优惠蜜语</th></tr>
-				<tr>
-					<td colspan="3"><label>
-						<input type="text" name="coupon-code" maxlength="15" placeholder="唯有美食与爱不可辜负" />
-					</label>
+			<tfoot role="rowgroup">
+				<tr role="row"><th role="rowheader" colspan="3">送餐费</th><td class="number">3</td></tr>
+				<tr role="row">
+					<th role="rowheader" colspan="3"><label>
+						优惠蜜语
+						<input
+							name="coupon-code"
+							type="text"
+							maxlength="15" />
+					</label></th>
 					<td class="number">-3</td>
 				</tr>
-				<tr><th colspan="3">送餐费</th><td class="number">3</td></tr>
-				<tr><th colspan="3">总计</th><td class="number">138</td></tr>
-				<tr><th colspan="4">
-					<label for="comment">备注
-						<input type="text" name="comment" maxlength="15" />
+				<tr role="row"><th role="rowheader" colspan="3">总计</th><td class="number">138</td></tr>
+				<tr role="row"><th role="rowheader" colspan="3">
+					<label for="comment">
+						备注
+						<input
+							type="text"
+							name="comment"
+							maxlength="15"/>
 					</label>
+				</th></tr>
+				<tr role="row"><th role="rowheader" colspan="3">
+					<fieldset>
+						<legend id="if-Preorder">预订</legend>
+						<label aria-hidden="true"><input type="checkbox" />预订</label><!--this dedicated label is for styling purpose-->
+						<input
+							aria-labelledby="if-Preorder"
+							type="datetime-local"
+							name="preorder-time"
+							step="600000"
+							bind:value={DateRange} {min} {max} />
+					</fieldset>
 				</th></tr>
 			</tfoot>
 		</table>
@@ -57,47 +76,50 @@
 				<div>
 					<dt>称呼
 					<dd><input
-						id="customer-name" type="text" name="customer-name" required maxlength="10" />
+						name="customer-name" 
+						type="text"
+						required
+						maxlength="10" />
 				</div>
 				<div>
-					<dt>手机
+					<dt>手机号
 					<dd><input
-						id="customer-tel" type="tel" name="customer-telephone" required maxlength="11" />
+						name="customer-telephone" 
+						type="tel"
+						required
+						maxlength="11" />
 				</div>
 				<div><dt>城市<dd>乌鲁木齐市</div>
 				<div>
 					<dt>地址
-					<dd><textarea name="customer-address" required maxlength="50" />
+					<dd><textarea
+						name="customer-address"
+						required
+						maxlength="50" />
 				</div>
 			</dl>
 		</address>
 
 		<fieldset class="payment">
 			<legend>付款方式</legend>
-
-			<fieldset>
-				<label>预订<input type="checkbox" /></label>
-				<fieldset>
-				<input
-					type="datetime-local"
-					name="preorder-time"
-					step="600000"
-					bind:value={DateRange} {min} {max}/>
-				</fieldset>
-			</fieldset>
-
 			<ol>
 				<li class="payment-input">
-					<input value="Cash" type="radio" name="payment" id="pay-by-cash" />
-					<label for="pay-by-cash">现金付</label>
+					<label>
+						<input value="Cash" type="radio" required name="payment" />
+						现金
+					</label>
 				</li>
 				<li class="payment-input">
-					<input value="wx_pub" type="radio" name="payment" id="pay-by-wx" />
-					<label for="pay-by-wx">微信支付</label>
+					<label>
+						<input value="alipay_wap" type="radio" required name="payment" />
+						支付宝
+					</label>
 				</li>
 				<li class="payment-input">
-					<input value="alipay_wap" type="radio" name="payment" id="pay-by-alipay" />
-					<label for="pay-by-alipay">支付宝</label>
+					<label>
+						<input value="wx_pub" type="radio" required name="payment" />
+						微信支付
+					</label>
 				</li>
 			</ol>
 		</fieldset>
@@ -112,14 +134,63 @@
 
 	/**Layout
    */
-	dialog {
+	.this\.cart {
 		position:static;
 		padding: var(--padding);
 	}
 
+	.this\.cart {
+		table {
+			--numr:4;
+			inline-size: min-content;
+			display: flex;
+			flex-flow: wrap;
+
+			> * { flex:100%; justify-content:space-between; }
+
+			thead, tbody {
+				inline-size: max-content;
+				display: flex;
+				flex-wrap: wrap;
+			}
+
+			tfoot {
+				display: flex;
+				flex-flow: wrap;
+
+				th[colspan="3"] { flex: calc(3 / var(--numr) * 100%); }
+				fieldset label { order:-1; display:flex; }
+			}
+
+			th:has(label,fieldset) { display:flex; flex-flow:wrap; /*inline-size:min-content;*/ }
+			th > fieldset legend { display:none; } //TODO: a11y
+			th > label input { order:1; }
+			th:has(label,fieldset) + td { align-self:flex-end; }
+
+			tr,
+			:is(td,th) > :is(label,fieldset) 
+			{ display:contents; }
+
+			tr::after,
+			:is(td,th) > :is(label,fieldset)::before
+			{ order:1; content:""; flex:100%; width:0; } // simulate <hr>
+		}
+
+		address :has(> dt + dd) { display:flex; flex-flow:column; }
+	}
+
 	/**Appearance
    */
-	.this\.cart  { color:inherit; border:0; }
-	.this\.cart h1 { visibility:var(--visibility); font-size:var(--s1); }
-	td.number { text-combine-upright:all; }
+	.this\.cart  {
+		color:inherit; border:0; background: var(--tabpanel_bg);
+
+		h1 { visibility:var(--visibility); font-size:var(--s1); }
+		td.number { text-combine-upright:all; }
+
+		legend#if-Preorder::after,
+		label:has(input:optional)::after { content:"（可选）"; position:relative; }
+
+		dt:has(+ dd > :is(input,textarea):required)::after { content:"*"; color:var(--alert_cl); }
+	}
+
 </style>
