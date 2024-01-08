97d23695f2e846ea1db14996e41ed4cd409a49ad
Sage <zongshian@outlook.com>
2024-01-08 13:21:52 +0800

    [UI] restructure <Cart> logic, simplify table flexbox layout;      nesting <input> element into <label> element is a bad idea.

--- src/lib/Cart.svelte
+++ src/lib/Cart.svelte
@@ -36,24 +36,24 @@
 			<tfoot role="rowgroup">
 				<tr role="row"><th role="rowheader" colspan="3">送餐费</th><td class="number">3</td></tr>
 				<tr role="row">
-					<th role="rowheader" colspan="3"><label>
-						优惠蜜语
-						<input
-							name="coupon-code"
-							type="text"
-							maxlength="15" />
-					</label></th>
+					<th role="rowheader" colspan="3">
+						<label for="coupon-code">优惠蜜语</label><input
+						id="coupon-code"
+						name="coupon-code"
+						type="text"
+						maxlength="15" />
+					</th>
 					<td class="number">-3
 				</tr>
 				<tr role="row"><th role="rowheader" colspan="3">总计</th><td class="number">138</td></tr>
 				<tr role="row">
-					<th role="rowheader" colspan="3"><label for="comment">
-						备注
-						<input
-							type="text"
+					<th role="rowheader" colspan="3">
+						<label for="comment">备注</label><input
+							id="comment"
 							name="comment"
+							type="text"
 							maxlength="15"/>
-					</label></th>
+						</th>
 					<td>
 				</tr>
 				<tr role="row">
@@ -75,30 +75,25 @@
 		<address>
 			配送信息
 			<dl>
-				<div>
-					<dt>称呼
-					<dd><input
-						name="customer-name" 
-						type="text"
-						required
-						maxlength="10" />
-				</div>
-				<div>
-					<dt>手机号
-					<dd><input
-						name="customer-telephone" 
-						type="tel"
-						required
-						maxlength="11" />
-				</div>
-				<div><dt>城市<dd>乌鲁木齐市</div>
-				<div>
-					<dt>地址
-					<dd><textarea
-						name="customer-address"
-						required
-						maxlength="50" />
-				</div>
+				<dt>城市
+				<dd>乌鲁木齐市
+				<dt>地址
+				<dd><input
+					name="customer-address"
+					required
+					maxlength="30" />
+				<dt>手机号
+				<dd><input
+					name="customer-telephone" 
+					type="tel"
+					required
+					maxlength="11" />
+				<dt>称呼
+				<dd><input
+					name="customer-name" 
+					type="text"
+					required
+					maxlength="10" />
 			</dl>
 		</address>
 
@@ -134,6 +129,9 @@
 	$numcol: 4;
 	/**Typesetting
    */
+	th,td,th > label { line-height:var(--line-height); }
+	tbody td:first-of-type { text-wrap:balance; text-align-last:right; }
+	dl > * { font-size:var(--s-2); line-height:var(--line-height); }
 
 	/**Layout
    */
@@ -141,73 +139,81 @@
 		position:static;
 		padding: var(--padding);
 
+		form > * + * {  margin-block-start:var(--s1); }
+		:is(table,table > *) > * + * {  margin-block-start:var(--s-2); }
+		thead { margin-block-start:0;} 
+		dl :nth-child(2n+3) {  margin-block-start:var(--s-2); }
+
+		form { --padding:.2em; --line-height:var(--s0); }
+
 		table {
 			--numcol: #{$numcol};
-			--numcol1:8; --numcol2:2; --numcol3:2; --numcol4:2;
+			--numcol1:5; --numcol2:2; --numcol3:2; --numcol4:2;
 			inline-size: min-content;
-			display: flex;
-			flex-flow: wrap;
+			display:flex; flex-flow:column;
 
-			> * { flex:100%; }//justify-content:space-between; }
-			tr > * { box-sizing: content-box; }
+			thead, tbody, tfoot {
+				display:flex; flex-flow:column;
+				justify-content: space-between;
+			}
 
 			thead, tbody {
-				inline-size: max-content;
-				display: flex;
-				flex-wrap: wrap;
-
 				@for $i from 1 to $numcol {
-					tr :nth-child(#{$i}) {
-						inline-size: calc(var(--numcol#{$i}) * 1em + (var(--numcol#{$i}) - 1) * var(--letter-spacing)); 
+					tr > :nth-child(#{$i}) { inline-size: calc(
+						var(--numcol#{$i}) * 1em + (var(--numcol#{$i}) - 1) * var(--letter-spacing));
 					}
 				}
 			}
 
-			tfoot {
-				display: flex;
-				flex-flow: wrap;
+			tr > :not(:first-child):last-child { inline-size: calc(
+				var(--numcol4) * 1em + (var(--numcol4) - 1) * var(--letter-spacing));
+			}
 
-				th[colspan="3"] {
-					flex:calc(3 / var(--numcol) * 100%);
-					> :is(label,fieldset) > * { flex-grow:1; }
+			tr     { display:flex; justify-content:space-between; }
+			tr > * { box-sizing:content-box; }
+			th, td { align-self:center; }
+			th:has(>label,>fieldset) { display:flex; flex-flow:column; }
+				
+			th > fieldset {
+				display:contents;
+				legend { display:none; } //TODO: a11y
+				label  {
+					order:-1;
+					display:flex;
+					input { order:-1; }
 				}
-
-				fieldset label { order:-1; display:flex; }
 			}
-
-			tr :not(:first-child):last-child {
-				inline-size: calc(var(--numcol4) * 1em + (var(--numcol4) - 1) * var(--letter-spacing));
-			}
-
-			th:has(label,fieldset) { display:flex; flex-flow:wrap; /*inline-size:min-content;*/ }
-			th > fieldset legend { display:none; } //TODO: a11y
-			th label input { order:1; }
-			th:has(label,fieldset) + td { align-self:flex-end; }
-
-			tr,
-			:is(td,th) > :is(label,fieldset) 
-			{ display:contents; }
-
-			tr::after,
-			:is(td,th) > :is(label,fieldset)::before
-			{ order:1; content:""; flex:100%; width:0; } // simulate <hr>
 		}
-
-		address :has(> dt + dd) { display:flex; flex-flow:column; }
+		
+		address dl dt,
+		table th > label { margin-block-end: calc(var(--line-height) * -1); }
+		
+		address dl dd input,
+		table th label + input { padding:var(--padding); background:transparent; }
 	}
 
 	/**Appearance
    */
 	.this\.cart  {
-		color:inherit; border:0; background: var(--tabpanel_bg);
+		border:0; color:inherit; background:var(--tabpanel_bg);
 
 		h1 { visibility:var(--visibility); font-size:var(--s1); }
 		td.number { text-combine-upright:all; }
 
-		legend#if-Preorder::after,
-		label:has(input:optional)::after { content:"（可选）"; position:relative; }
+		dl dd :is(input,textarea),
+		th > label + input,
+		th fieldset label + input
+		{
+			border: 0;
+			outline: var(--primary_cl) solid 1px;
+			padding-block-start: .5em;
+			padding-block-end: .25em;
+		}
 
-		dt:has(+ dd > :is(input,textarea):required)::after { content:"*"; color:var(--alert_cl); }
+		legend#if-Preorder::after,
+		label:has(+ input:optional)::after { content:"（可选）"; }
+
+		dt:has(+ dd > input:required)::after { content:"*"; color:var(--alert_cl); }
 	}
 
 </style>
